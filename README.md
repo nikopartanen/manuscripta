82bd45a1d246dece5c2998535a44acc15cb2f1fc
Niko Partanen <nikotapiopartanen@gmail.com>
2021-12-18 16:22:40 +0200

    readme

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Manuscripta
 
-This is a Python package that contains various functions to process and evaluate text recognition materials. It supports currently Page XML and Alto XML formats. The package is intended to be used in connection with other Python packages that offer higher level functionality, such as [UralicNLP](https://github.com/mikahama/uralicnlp) and [murre](https://github.com/mikahama/murre).
+The original idea was to structure the code in this repository as a Python package. This is, however, still unfinished. The repository contains now three larger example scripts that address different work phases when working with text recognition materials, especially in the context of [Transkribus](https://transkribus.eu/). The main idea is to extract the data and manipulate it so that it can be further processed with different NLP libraries, such as [UralicNLP](https://github.com/mikahama/uralicnlp) and [murre](https://github.com/mikahama/murre).
 
 ## Functionality
 
-### Reading OCR XML files
+### Editing Page XML files
 
-Function `read_ocr_xml()` can be used to read two commonly used XML types with returned list of dictionaries.
+Script `processing_page_xml.py` contains an example file used in Manuscripta Castreniana project to edit some of the proofread files. The same mechanism can be extended to different tasks. 
 
-### Visualization
+### Transkribus API
 
-Function `plot_page()` takes arguments `xml_file` an `image_file`, and returns an image with the layout elements highlighted.
+Script `transkribus_api.py` contains examples of how to use Transkribus API to investigate user collections and to retrieve documents. There is an example about saving all ground truth files into their own directory for further manipulation. 
+
+### Editing Ground Truth data
+
+Script `edit_ground_truth.py` contains examples of extracting the line polygons from Page XML, visualizing their locations with different parameters, and collecting individual lines into OCR model training experiments. The current structure 
 
 ### Transliteration
 
-It is extremely common that transcribed materials are in very rare writing systems. The package contains few transliteration patterns that have been useful in our work.
+It is an extremely common issue that manuscripts or printed books contain texts in endangered languages in writing or transcription systems that are not widely used, possibly being used only in these documents. One convention our research group has used is transliteration, so that we apply rule based patterns into the original text, bringing it closer to current orthography. This works often very well when we operate on phonemic transcription systems in languages that have phonemic orthographies. Script `lat2cyr_kpv.py` illustrates this in Komi context.
+
---
+++ transkribus_api.py
@@ -0,0 +1,86 @@
+# This is an example of how to use Transkribus API
+# It is copied and modified from somewhere in GitHub 
+# and Transkribus API's documentation. Thanks for everyone
+# whose examples have been used!
+
+import requests
+from lxml import objectify
+from pathlib import Path
+
+# Define building blocks for the endpoints:
+api_base_url = "https://transkribus.eu/TrpServer/rest/"
+login_endpoint = "auth/login"
+collections_endpoint = "collections/list"
+logout_endpoint = "auth/logout"
+
+username = '' # email address you use to login to Transkribus
+password = '' # maybe come up with some other way to store the password
+
+# Log in:
+## Prepare the payload for the POST request:
+credentials = {'user': username,
+               'pw': password}
+## Create the POST request (the requests package converts the credentials into JSON automatically):
+response = requests.post(api_base_url + login_endpoint, data=credentials)
+
+if response:
+    r = objectify.fromstring(response.content)
+    print(f"TRANSKRIBUS: User {r.firstname} {r.lastname} ({r.userId}) logged in successfully.")
+    session_id = str(r.sessionId)
+else:
+    sys.exit("TRANSKRIBUS: Login failed. Check your credentials!")
+    
+# View your collections
+
+cookies = dict(JSESSIONID=session_id)
+response = requests.get(api_base_url + collections_endpoint, cookies=cookies)
+if response:
+    collections = response.json()
+    print("Your collections:")
+    for collection in collections:
+#        print(collection)
+        print(f"– {collection['colName']}: {collection['colId']}")
+else:
+    sys.exit("Could not retrieve collections.")
+
+# Select the collection id you are interested about and view the documents in it
+
+col_id = '0000'
+
+response = requests.get(f"https://transkribus.eu/TrpServer/rest/collections/{col_id}/list", cookies=cookies)
+
+for document in response.json():
+    
+    print(f"{document['title']}: {document['docId']}")
+
+# This would be the way to get the document object
+
+doc_id = '0000'
+
+document = requests.get(f"https://transkribus.eu/TrpServer/rest/collections/{doc_id}/{col_id}/fulldoc", cookies=cookies)
+
+# Let's say we want to save all ground truth pages as files, as we often want to do 
+# when we train models locally
+
+def write_gt(document, directory = 'ground_truth'):
+
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    for page in document.json()['pageList']['pages']:
+
+        file = f"{directory}/{page['imgFileName']}"
+
+        if page['tsList']['transcripts'][0]['status'] == 'GT' and not Path(file).is_file():
+
+            image = requests.get(page['url'], allow_redirects=True)
+
+            open(file, 'wb').write(image.content)
+
+        if page['tsList']['transcripts'][0]['status'] == 'GT':
+
+            transcript = requests.get(page['tsList']['transcripts'][0]['url'], allow_redirects=True)
+
+            open(Path(file).with_suffix('.xml'), 'wb').write(transcript.content)
+
+# Example call
+write_gt(document)
